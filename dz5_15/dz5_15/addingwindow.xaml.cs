97a0b85dff0a4d42df835c850cc0f323b0256acf
nnnagato <85846084+nnnagato@users.noreply.github.com>
2022-11-14 00:28:32 +0300

    вариант 15

---
+++ dz5_15/dz5_15/addingwindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dz5_15
+{
+    /// <summary>
+    /// Логика взаимодействия для addingwindow.xaml
+    /// </summary>
+    public partial class addingwindow : Window
+    {
+        public addingwindow()
+        {
+            InitializeComponent();
+        }
+        public int article;
+        public int size;
+        public int price;
+
+        int get_value(int index)
+        {
+            int buf;
+            int result;
+            TextBox txt = sizeBox;
+            switch(index)
+            {
+                case 1:
+                    txt = articulBox;
+                    break;
+                case 2:
+                    txt = sizeBox;
+                    break;
+                case 3:
+                    txt = priceBox;
+                    break;
+            }
+            if(int.TryParse(txt.Text, out buf))
+            {
+                if(buf>0)
+                {
+                    result = buf;
+                }
+                else
+                {
+                    MessageBox.Show("Введите положительные значения");
+                    throw new Exception("Ошибка ввода");
+                }    
+            }
+            else
+            {
+                MessageBox.Show("Введите во все поля целые числа");
+                throw new Exception("Ошибка ввода");
+            }
+            return result;
+        }
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            article = get_value(1);
+            size = get_value(2);
+            price = get_value(3);
+            this.DialogResult = true;
+        }
+
+        private void denyButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
---
+++ dz5_15/dz5_15/changingwindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dz5_15
+{
+    /// <summary>
+    /// Логика взаимодействия для extrawindow.xaml
+    /// </summary>
+    public partial class extrawindow : Window
+    {
+        public extrawindow()
+        {
+            InitializeComponent();
+        }
+
+        public int get_value(int box)
+        {
+            int buf;
+            int rezult;
+            TextBox txt = numBox;
+            switch(box)
+            {
+                case 1:
+                    txt = numBox;
+                    break;
+                case 2:
+                    txt = articulBox;
+                    break;
+                case 3:
+                    txt = sizeBox;
+                    break;
+                case 4:
+                    txt = priceBox;
+                    break;
+            }
+            
+            if(int.TryParse(txt.Text, out buf))
+            {
+                if(buf > 0)
+                {
+                    rezult = buf;                        
+                }
+                else
+                {
+                    MessageBox.Show("Укажите во всех строка положительные целочисленные значения.");
+                    throw new Exception("ошибка ввода");
+                }
+            }
+            else
+            {
+                 MessageBox.Show("Укажите во всех строка положительные целочисленные значения.");
+                 throw new Exception("ошибка ввода");
+            }
+            
+            return rezult;
+        }
+        public int number;
+        public int article;
+        public int size;
+        public int price;
+        
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            number = get_value(1);
+            article = get_value(2);
+            size = get_value(3);
+            price = get_value(4);
+            this.DialogResult = true;
+        }
+        private void denyButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
